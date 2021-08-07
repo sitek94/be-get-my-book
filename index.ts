051ac2fdda1cb76767f9694fd313f616c3be6e60
Maciek <msitkowski94@gmail.com>
2021-08-07 08:40:07 +0200

    feat: scrape lubimyczytac to get an author

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world, again!");
+import puppeteer from "puppeteer";
+
+const url =
+  "https://lubimyczytac.pl/ksiazka/4909770/milczacy-przewodnicy-jak-rozumiec-i-doskonalic-swoj-umysl";
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  const element = await page.$("span.author");
+
+  const text = await page.evaluate((element) => element.textContent, element);
+  console.log(text.trim());
+
+  await browser.close();
+})();
